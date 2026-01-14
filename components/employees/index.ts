c36eb4a6b83681e0e029c4c839aaf710b370846b
Injamul Islam <injamulislam132@gmail.com>
2026-01-14 14:18:18 +0600

    fix: fixed build error

--- components/employees/index.ts
+++ components/employees/index.ts
@@ -1,5 +1,4 @@
 export { EmployeeDirectoryHeader } from "./employee-directory-header";
-export { EmployeeSearch } from "./employee-search";
 export { EmployeeTable } from "./employee-table";
 export { EmployeeScheduleModal } from "./employee-schedule-modal";
 export type { Employee, BiometricStatus, AttendanceStatus } from "./employee-table";
